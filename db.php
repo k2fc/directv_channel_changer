9bb153e57c010e4d6da9c8766c30e06b8d8416e7
Dennis Graiani <dennis.graiani@gmail.com>
2018-02-22 15:21:24 +0000

    Initialize new database

--- db.php
+++ db.php
@@ -1,3 +1,8 @@
 <?php
 $db = new SQLite3('tuners.db');
+
+$db->query("CREATE TABLE IF NOT EXISTS interfaces (Name Varchar, Address int, SubnetMask int)");
+$db->query("CREATE TABLE IF NOT EXISTS tuners (serialNum Varchar, name Varchar, address Int)");
+$db->query("CREATE TABLE IF NOT EXISTS settings (setting Varchar, value Varchar)");
+
 ?>
